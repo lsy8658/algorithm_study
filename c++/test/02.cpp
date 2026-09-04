d4f528cab7500d8a775f8b96b34cfae5dd3e5854
lsy8658 <socszz@gmail.com>
2026-09-04 10:54:07 +0900

    순열

--- c++/test/02.cpp
+++ c++/test/02.cpp
@@ -2,9 +2,18 @@
 
 using namespace std;
 
-int main () {
-    int a[] = {1,2,3};
+// int main () {
+//     int a[] = {1,2,3};
 
+//     do {
+//         for (int i : a) cout << i << " ";
+//         cout << "\n";
+//     } while (next_permutation(&a[0], &a[0] + 3));
+// }
+
+int main () {
+    vector<int> a = {1,2,3};
+    sort(a.begin(), a.end());
     do {
         for (int i : a) cout << i << " ";
         cout << "\n";
--- c++/test/exam/make_permutation.cpp
+++ c++/test/exam/make_permutation.cpp
@@ -1,30 +1,35 @@
-#include<bits/stdc++.h>
+#include<stdc++.h>
+
 using namespace std;
 
-vector<int> v = {1, 2, 3};
+vector<int> v;
 
 void printV (vector<int> &v) {
+   
     for (int i = 0; i < v.size(); i++) {
         cout << v[i] << " ";
     }
+
     cout << "\n";
 }
 
-void makePermutation (int n, int r, int depth) {
-    if (r == depth) {
+
+void make_permutation (int n, int r, int depth) {
+    if (n == depth) {
         printV(v);
-        return;
+        return; 
     }
 
     for (int i = depth; i < n; i++) {
         swap(v[i], v[depth]);
-        makePermutation(n, r, depth + 1);
+        make_permutation(n, r, depth + 1);
         swap(v[i], v[depth]);
     }
 }
 
 int main () {
-    makePermutation(3,3,0);
-
-    return 0;
+    for (int i = 1; i <= 3; i++)  {
+        v.push_back(i);
+    }
+    make_permutation(3,3,0);
 }
